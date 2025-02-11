f5c54f814363796303ffbca235138e2300b73c31
uzma shaik <uzma97910@gmail.com>
2025-02-11 14:57:31 +0100

    feat: display character list

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function HomePage() {
-  return <h1>List of Characters</h1>;
+  const [characters, setCharacters] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://ih-crud-api.herokuapp.com/characters")
+      .then((response) => {
+        setCharacters(response.data);
+      })
+      .catch((e) => {
+        console.log("Error", e);
+      });
+  }, []);
+
+  if (characters === null) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div>
+      <h3>Number of Characters in the API : {characters.length} </h3>
+      {characters.map((characterObj, i) => {
+        return (
+          <div key={i} className="box">
+            <h4>Name : {characterObj.name}</h4>
+            <p>Occupation : {characterObj.occupation}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default HomePage;
